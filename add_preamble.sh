05357edce756a1dfd25e6a73482957c3b94f96d2
Dhaval Anjaria <dhavalmanjaria@gmail.com>
2017-02-11 11:53:48 +0530

    Changed preamble script to skip files if they already have it

--- add_preamble.sh
+++ add_preamble.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-for x in `find . -name '*.py'`; do cat /home/dhaval/Documents/Python/Django/preamble.py $x | tee $x.bk; cat $x.bk > $x; done 
+
+for x in `find . -name '*.py'`; do
+        if [[ $(cat $x | grep 'gnu.org') ]]; then
+            continue
+        fi
+        cat /home/dhaval/Documents/Python/Django/preamble.py $x | tee $x.bk
+        cat $x.bk > $x    
+    done 
 
 for x in `find . -name '*.bk'`; do rm $x; done
