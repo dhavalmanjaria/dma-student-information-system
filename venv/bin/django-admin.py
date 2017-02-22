50de0c7ccd405a96ecee9e3731273e6ac63ab4b3
Dhaval Anjaria <dhavalmanjaria@gmail.com>
2017-02-23 01:04:47 +0530

    Changed boostrap links to https

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dhaval/Documents/Python/Django/student_information_system/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
