4eb7b35c796add17bc86e904cda461803be51974
Dhaval Anjaria <dhavalmanjaria@gmail.com>
2017-03-25 17:04:27 +0530

    Fixed bug where unauthorized users could create notices

--- notices/views.py
+++ notices/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Notice
 from django.views import generic
 from .forms import CreateNoticeForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 
 
@@ -9,7 +10,7 @@ class NoticesListView(generic.ListView):
     paginate_by = 10
     model = Notice
 
-class CreateNotice(generic.edit.CreateView):
+class CreateNotice(generic.edit.CreateView, LoginRequiredMixin, PermissionRequiredMixin):
     model = Notice
     fields = '__all__'
     success_url = reverse_lazy('notices')
@@ -17,11 +18,17 @@ class CreateNotice(generic.edit.CreateView):
     def get_queryset(self):
         return Notice.objects.all().order_by(date)
 
+    class Meta:
+        permissions_required = ('can_write_notices', )
+
 
 class NoticeDetailView(generic.DetailView):
     model = Notice
 
 
-class UpdateNotice(generic.UpdateView):
+class UpdateNotice(generic.UpdateView, LoginRequiredMixin, PermissionRequiredMixin):
     model = Notice
     fields = '__all__'
+
+    class Meta:
+        permissions_required = ('can_write_notices', )
